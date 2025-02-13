0d4e49c7e8274d83878afeb2a61feeb751405797
MarieDevulder <marie.devulder@student.junia.com>
2025-02-13 13:59:52 +0100

    lint: precommit run

--- models/converted.py
+++ models/converted.py
@@ -1,8 +1,8 @@
 import numpy as np
 import onnxruntime as rt
+from pydantic import BaseModel
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from pydantic import BaseModel
 
 X, y = datasets.load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(
@@ -21,20 +21,27 @@ print("prediction:", pred_onx)
 
 
 class Iris(BaseModel):
-  sepal_length: float
-  sepal_width: float
-  petal_length: float
-  petal_width: float
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
 
 
 iris_value = Iris(
-  sepal_length=1,
-  sepal_width=0.4,
-  petal_length=0.685,
-  petal_width=2,
+    sepal_length=1,
+    sepal_width=0.4,
+    petal_length=0.685,
+    petal_width=2,
 )
 
-iris = np.array([iris_value.sepal_length,iris_value.sepal_width,iris_value.petal_length,iris_value.petal_width]).reshape(-1,4)
+iris = np.array(
+    [
+        iris_value.sepal_length,
+        iris_value.sepal_width,
+        iris_value.petal_length,
+        iris_value.petal_width,
+    ]
+).reshape(-1, 4)
 
 pred_onx = sess.run([prob_name], {input_name: iris.astype(np.float32)})
 print("prediction:", pred_onx)
